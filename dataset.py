9e31fe0b26fda0b7b6bb59aa207e7285fda012dd
annejooyun <ajmarth@stud.ntnu.no>
2023-02-21 14:50:36 +0100

    it works?

--- dataset.py
+++ dataset.py
@@ -3,11 +3,27 @@ import os
 import numpy as np
 import pandas as pd
 import variables as v
+import logging
+import mne
+from sklearn.model_selection import StratifiedKFold
 
-def read_eeg_data(dir, filename):
-    data_key = 'raw_eeg_data'
-    f = dir + filename
-    return scipy.io.loadmat(f)[data_key]
+def read_eeg_data(data_type, filename):
+    print('im in')
+    if data_type == 'raw':
+        data_key = 'raw_eeg_data'
+    elif data_type == 'ica':
+        data_key = 'Clean_data'
+    else:
+        print(f'No data with data_type = {data_type} found')
+        return 0
+    print(filename)
+    data = scipy.io.loadmat(filename)[data_key]
+    info = mne.create_info(8, sfreq=v.SFREQ, ch_types= 'eeg', verbose=None)
+    raw_arr = mne.io.RawArray(data, info)
+    mapping = {'0':'F4','1':'Fp2','2':'C3','3':'FC6','4':'O1','5':'Oz','6':'FT9','7':'T8'}
+    mne.rename_channels(raw_arr.info, mapping)
+
+    return raw_arr
 
 def generate_all_recs():
     """
@@ -30,7 +46,7 @@ def generate_all_recs():
     return recs
 
 
-def filter_valid_recs(recs):
+def filter_valid_recs(recs, data_type = 'ica'):
     """
     This function returns the valid recordings from a list of recordings.
     Parameters
@@ -42,19 +58,25 @@ def filter_valid_recs(recs):
     valid_recs : list
         A list of valid recordings in the format 'P00X_S00X_00X'.
     """
+    if data_type == 'raw':
+        dir = v.DIR_RAW
+    elif data_type == 'ica':
+        dir = v.DIR_ICA_FILTERED
+    else:
+        print(f'No data with data_type = {data_type} found')
+        return 0
 
-    dir = v.DIR_RAW
     valid_recs = []
     for rec in recs:
         subject, session, run = rec.split('_')
         f_name = os.path.join(
             dir, f'sub-{subject}_ses-{session}_run-{run}.mat')
         try:
-            data = read_eeg_data(f_name)
+            data = read_eeg_data(data_type, f_name)
             if data.n_times / data.info['sfreq'] >= 4.30 * 60:
                 valid_recs.append(rec)
         except:
-            logging.error(f"Failed to read data for recording {rec}")
+            logging.error(f"1) Failed to read data for recording {rec}")
             continue
     return valid_recs
 
@@ -73,14 +95,13 @@ def get_valid_recs():
     return valid_recs
 
 
-def load_dataset(data_type="ica"):
+def extract_eeg_data(valid_recs, data_type="ica"):
     '''
     Loads data from the dataset.
     The data_type parameter specifies which of the datasets to load. Possible values
-    are raw, filtered, ica_filtered.
-    Returns a Numpy Array with shape (120, 32, 3200).
+    are raw and ica_filtered.
+    Returns
     '''
-
     assert (data_type in v.DATA_TYPES)
 
     if data_type == "raw":
@@ -93,29 +114,19 @@ def load_dataset(data_type="ica"):
         print("No files matching data type found")
         return 0
 
-    #dataset = np.empty((v.NUM_SUBJECTS, v.NUM_CHANNELS, v.SFREQ*5*60)) #5 minute recorings
-    dataset = {}
-
-    for filename in os.listdir(dir):
-        f = os.path.join(dir, filename)
-        # checking if it is a file
-        if os.path.isfile(f):
-            print(f)
-        key = 
-        dataset
-
-
-    counter = 0
-
-    for filename in os.listdir(dir):
-        if test_type not in filename:
-            continue
-
-        f = os.path.join(dir, filename)
-        data = scipy.io.loadmat(f)[data_key]
-        dataset[counter] = data
-        counter += 1
-    return dataset
+    eeg_data = {}
+    for rec in valid_recs:
+        subject, session, run = rec.split('_')
+        f_name = os.path.join(
+            dir, f'sub-{subject}_ses-{session}_run-{run}.mat')
+        try:
+            data = read_eeg_data(data_type, f_name)
+        except:
+            logging.error(f"2) Failed to read data for recording {rec}")
+            data = None
+        key = f"{subject}_{session}_{run}"
+        eeg_data[key] = data
+    return eeg_data
 
 
 def compute_average_scores(path='Data/STAI_grading.xlsx'):
@@ -185,15 +196,15 @@ def compute_score_labels(scores, low_cutoff, high_cutoff):
 
     labels = {}
     for i in range(scores.shape[0]):
-        for j in range(v.N_SESSIONS*v.N_RUNS):
+        for j in range(v.NUM_SESSIONS*v.NUM_RUNS):
 
             label = int(scores.iloc[i, j+8]
                         in range(low_cutoff, high_cutoff+1))
             if scores.iloc[i, j+8] > high_cutoff:
                 label = 2
             subject = i + 1
-            session = j//v.N_RUNS + 1
-            run = j % v.N_RUNS + 1
+            session = j//v.NUM_RUNS + 1
+            run = j % v.NUM_RUNS + 1
             key = f'P{str(subject).zfill(3)}_S{str(session).zfill(3)}_{str(run).zfill(3)}'
             labels[key] = label
     return labels
@@ -223,30 +234,86 @@ def get_labels(valid_recs, path='Data/STAI_grading.xlsx', low_cutoff=37, high_cu
 
 
 
-def convert_to_epochs(dataset, channels, sfreq):
-    '''
-    Splits EEG data into epochs with length 1 sec
-    '''
-    epoched_dataset = np.empty((dataset.shape[0], dataset.shape[2]//sfreq, channels, sfreq))
-    for i in range(dataset.shape[0]):
-        for j in range(dataset.shape[2]//sfreq):
-            epoched_dataset[i, j] = dataset[i, :, j*sfreq:(j+1)*sfreq]
-    return epoched_dataset
+def extract_epochs(x_dict, y_dict, epoch_duration=3):
+    """
+    Extracts epochs of given duration from each sample in x_dict.
+    Parameters
+    ----------
+    x_dict : dict
+        Dictionary of MNE raw objects, where each object is a sample.
+    y_dict : dict
+        Dictionary of labels for each sample in x_dict.
+    epoch_duration : float, optional
+        Duration of each epoch, in seconds, by default 3.
+    Returns
+    -------
+    x_epochs : dict
+        A dictionary of MNE Epoch objects, where each sample in x_epochs is split into epochs of the given duration.
+    y_epochs : dict
+        A dictionary of labels for each epoch in x_epochs.
+    """
+
+    overlap_duration = 0.0  # in seconds
+
+    x_epochs = {}
+    y_epochs = {}
+
+    for key, raw in x_dict.items():
+        events = mne.make_fixed_length_events(
+            raw, duration=epoch_duration, overlap=overlap_duration)
+        epochs = mne.Epochs(raw, events, tmin=0,
+                            tmax=epoch_duration, baseline=None, preload=True)
+
+        for i, epoch in enumerate(epochs):
+            x_epochs[f"{key}_epoch{i}"] = epoch
+            y_epochs[f"{key}_epoch{i}"] = y_dict[key]
+
+    return x_epochs, y_epochs
 
 
-def load_channels():
-        '''
-        Loads the channel names from the file Coordinates.locs
-        '''
-        root = 'Data'
-        coordinates_file = os.path.join(root,"Coordinates.locs") 
+def kfold_split(x_epochs, y_epochs, n_splits=5, shuffle=True, random_state=None):
+    """
+    Perform k-fold cross-validation on a dataset of EEG epochs.
+    Parameters
+    ----------
+    x_epochs : dict
+        A dictionary of EEG epochs, where each key is an epoch ID and each value is a numpy array of shape (n_channels, n_samples).
+    y_epochs : dict
+        A dictionary of target labels for each epoch, where each key is an epoch ID and each value is an integer target label.
+    n_splits : int, optional
+        The number of folds to create in the cross-validation. Default is 5.
+    shuffle : bool, optional
+        Whether to shuffle the data before creating folds. Default is True.
+    random_state : int or RandomState, optional
+        If an integer, `random_state` is the seed used by the random number generator. If a RandomState instance, `random_state` is the random number generator. If None, the random number generator is the RandomState instance used by `np.random`. Default is None.
+    Returns
+    -------
+    train_epochs : list of dicts
+        A list of training epochs for each fold, where each dictionary is of the same format as `x_epochs`.
+    test_epochs : list of dicts
+        A list of test epochs for each fold, where each dictionary is of the same format as `x_epochs`.
+    train_labels : list of dicts
+        A list of target labels for the training epochs for each fold, where each dictionary is of the same format as `y_epochs`.
+    test_labels : list of dicts
+        A list of target labels for the test epochs for each fold, where each dictionary is of the same format as `y_epochs`.
+    """
 
-        channel_names = []
+    subject_ids = np.unique(['_'.join(k.split('_')[:-1]) for k in x_epochs.keys()])
+    y_subjects = np.array([y_epochs[f'{subject_id}_epoch0'] for subject_id in subject_ids])
 
-        with open(coordinates_file, "r") as file:
-            for line in file:
-                elements = line.split()
-                channel = elements[-1]
-                channel_names.append(channel)
-                
-        return channel_names
+    skf = StratifiedKFold(n_splits=n_splits, shuffle=shuffle, random_state=random_state)
+
+    train_epochs = []
+    train_labels = []
+    test_epochs = []
+    test_labels = []
+    for train_subjects, test_subjects in skf.split(subject_ids, y_subjects):
+        train_subjects_list = subject_ids[train_subjects]
+        test_subjects_list = subject_ids[test_subjects]
+        train_epochs.append({k: v for k, v in x_epochs.items() if '_'.join(k.split('_')[:-1]) in train_subjects_list})
+        train_labels.append({k: v for k, v in y_epochs.items() if '_'.join(k.split('_')[:-1]) in train_subjects_list})
+        test_epochs.append({k: v for k, v in x_epochs.items() if '_'.join(k.split('_')[:-1]) in test_subjects_list})
+        test_labels.append({k: v for k, v in y_epochs.items() if '_'.join(k.split('_')[:-1]) in test_subjects_list})
+
+    return train_epochs, test_epochs, train_labels, test_labels
+
