e97420bd52ce98afb1905c1f3cb4cd290fe79c5e
annejooyun <ajmarth@stud.ntnu.no>
2023-02-23 12:20:47 +0100

    added to data and labels

---
+++ utils/data.py
@@ -0,0 +1,141 @@
+import mne
+import os
+import numpy as np
+import scipy
+from sklearn.model_selection import StratifiedKFold
+import logging
+import utils.variables as v
+
+
+def read_eeg_data(data_type, filename):
+    if data_type == 'raw':
+        data_key = 'raw_eeg_data'
+    elif data_type == 'ica':
+        data_key = 'Clean_data'
+    else:
+        print(f'No data with data_type = {data_type} found')
+        return 0
+    data = scipy.io.loadmat(filename)[data_key]
+    info = mne.create_info(8, sfreq=v.SFREQ, ch_types= 'eeg', verbose=None)
+    raw_arr = mne.io.RawArray(data, info)
+    mapping = {'0':'F4','1':'Fp2','2':'C3','3':'FC6','4':'O1','5':'Oz','6':'FT9','7':'T8'}
+    mne.rename_channels(raw_arr.info, mapping)
+
+    return raw_arr
+
+
+def extract_eeg_data(valid_recs, data_type):
+    '''
+    Loads data from the dataset.
+    The data_type parameter specifies which of the datasets to load. Possible values
+    are raw and ica_filtered.
+    Returns
+    '''
+    assert (data_type in v.DATA_TYPES)
+
+    if data_type == "raw":
+        dir = v.DIR_RAW
+        data_key = 'Data'
+    elif data_type == "ica":
+        dir = v.DIR_ICA_FILTERED
+        data_key = 'Clean_data'
+    else:
+        print("No files matching data type found")
+        return 0
+
+    eeg_data = {}
+    for rec in valid_recs:
+        subject, session, run = rec.split('_')
+        f_name = os.path.join(
+            dir, f'sub-{subject}_ses-{session}_run-{run}.mat')
+        try:
+            data = read_eeg_data(data_type, f_name)
+        except:
+            logging.error(f"2) Failed to read data for recording {rec}")
+            data = None
+        key = f"{subject}_{session}_{run}"
+        eeg_data[key] = data
+    return eeg_data
+
+def segment_data(x_dict, y_dict, epoch_duration=3):
+    """
+    Extracts epochs of given duration from each sample in x_dict.
+    Parameters
+    ----------
+    x_dict : dict
+        Dictionary of MNE raw objects, where each object is a sample.
+    y_dict : dict
+        Dictionary of labels for each sample in x_dict.
+    epoch_duration : float, optional
+        Duration of each epoch, in seconds, by default 3.
+    Returns
+    -------
+    x_epochs : dict
+        A dictionary of MNE Epoch objects, where each sample in x_epochs is split into epochs of the given duration.
+    y_epochs : dict
+        A dictionary of labels for each epoch in x_epochs.
+    """
+
+    overlap_duration = 0.0  # in seconds
+
+    x_epochs = {}
+    y_epochs = {}
+
+    for key, raw in x_dict.items():
+        events = mne.make_fixed_length_events(
+            raw, stop = 5*60, duration=epoch_duration, overlap=overlap_duration)
+        epochs = mne.Epochs(raw, events, tmin=0, tmax=epoch_duration, baseline=None, preload=True)
+
+        for i, epoch in enumerate(epochs):
+            x_epochs[f"{key}_epoch{i}"] = epoch
+            y_epochs[f"{key}_epoch{i}"] = y_dict[key]
+
+    return x_epochs, y_epochs
+
+def kfold_split(x_epochs, y_epochs, n_splits=5, shuffle=True, random_state=None):
+    """
+    Perform k-fold cross-validation on a dataset of EEG epochs.
+    Parameters
+    ----------
+    x_epochs : dict
+        A dictionary of EEG epochs, where each key is an epoch ID and each value is a numpy array of shape (n_channels, n_samples).
+    y_epochs : dict
+        A dictionary of target labels for each epoch, where each key is an epoch ID and each value is an integer target label.
+    n_splits : int, optional
+        The number of folds to create in the cross-validation. Default is 5.
+    shuffle : bool, optional
+        Whether to shuffle the data before creating folds. Default is True.
+    random_state : int or RandomState, optional
+        If an integer, `random_state` is the seed used by the random number generator. If a RandomState instance, `random_state` is the random number generator. If None, the random number generator is the RandomState instance used by `np.random`. Default is None.
+    Returns
+    -------
+    train_epochs : list of dicts
+        A list of training epochs for each fold, where each dictionary is of the same format as `x_epochs`.
+    test_epochs : list of dicts
+        A list of test epochs for each fold, where each dictionary is of the same format as `x_epochs`.
+    train_labels : list of dicts
+        A list of target labels for the training epochs for each fold, where each dictionary is of the same format as `y_epochs`.
+    test_labels : list of dicts
+        A list of target labels for the test epochs for each fold, where each dictionary is of the same format as `y_epochs`.
+    """
+
+    subject_ids = np.unique(['_'.join(k.split('_')[:-1]) for k in x_epochs.keys()])
+    y_subjects = np.array([y_epochs[f'{subject_id}_epoch0'] for subject_id in subject_ids])
+
+    skf = StratifiedKFold(n_splits=n_splits, shuffle=shuffle, random_state=random_state)
+
+    train_epochs = []
+    train_labels = []
+    test_epochs = []
+    test_labels = []
+    for train_subjects, test_subjects in skf.split(subject_ids, y_subjects):
+        train_subjects_list = subject_ids[train_subjects]
+        test_subjects_list = subject_ids[test_subjects]
+        train_epochs.append({k: v for k, v in x_epochs.items() if '_'.join(k.split('_')[:-1]) in train_subjects_list})
+        train_labels.append({k: v for k, v in y_epochs.items() if '_'.join(k.split('_')[:-1]) in train_subjects_list})
+        test_epochs.append({k: v for k, v in x_epochs.items() if '_'.join(k.split('_')[:-1]) in test_subjects_list})
+        test_labels.append({k: v for k, v in y_epochs.items() if '_'.join(k.split('_')[:-1]) in test_subjects_list})
+
+    return train_epochs, test_epochs, train_labels, test_labels
+
+
---
+++ utils/labels.py
@@ -0,0 +1,181 @@
+214 lines (181 sloc)  7.04 KB
+import numpy as np
+import pandas as pd
+import logging
+import utils.variables as v
+
+def load_pss_labels(filename, threshold):
+    """
+    Load and binarize PSS (Perceived Stress Scale) scores from an Excel file.
+    Parameters
+    ----------
+    filename : str
+        The path to the Excel file containing the PSS scores.
+    threshold : int or float
+        The threshold value to use for binarizing the PSS scores. Scores greater than `threshold` will be
+        binarized to 1, and scores less than or equal to `threshold` will be binarized to 0.
+    Returns
+    -------
+    scores : pandas.DataFrame
+        A DataFrame containing the binarized PSS scores, where each row represents a subject and each
+        column represents a session. Scores are binarized according to the `threshold` value.
+    """
+    scores = pd.read_excel(filename, sheet_name='Rating 1-10', skiprows=[1])
+    scores.iloc[:, 1:] = scores.iloc[:, 1:].applymap(
+        lambda x: x if pd.isna(x) else x > threshold)
+    return scores
+
+
+def filter_pss_labels(scores, valid_recs):
+    """
+    Filter PSS (Perceived Stress Scale) scores by record ID.
+    Parameters
+    ----------
+    scores : pandas.DataFrame
+        A DataFrame containing PSS scores for each subject and session.
+    valid_recs : list of str
+        A list of valid record IDs to keep in the filtered scores.
+    Returns
+    -------
+    filtered_labels : dict
+        A dictionary containing the filtered PSS scores, where the keys are record IDs and the values are the corresponding scores.
+    """
+    scores_dict = {}
+    for i, _ in scores.iterrows():
+        for j in range(len(scores.columns)):
+            session_no = j // 2 + 1
+            run_no = j % 2 + 1
+            subject = f'P{str(i+1).zfill(3)}'
+            session = f'S{str(session_no).zfill(3)}'
+            run = f'{str(run_no).zfill(3)}'
+            rec = f'{subject}_{session}_{run}'
+            scores_dict[rec] = scores.iloc[i, j]
+
+    filtered_labels = {rec: scores_dict[rec] for rec in valid_recs}
+    return filtered_labels
+
+
+def get_pss_labels(valid_recs, filename='Data/STAI_grading.xlsx', threshold=4):
+    """
+    Get filtered and binarized PSS (Perceived Stress Scale) scores for a list of valid record IDs.
+    Parameters
+    ----------
+    valid_recs : list of str
+        A list of valid record IDs to keep in the filtered scores.
+    filename : str, optional
+        The path to the Excel file containing the PSS scores. Default is 'Data/STAI_grading.xlsx'.
+    threshold : int or float, optional
+        The threshold value to use for binarizing the PSS scores. Scores greater than `threshold` will be
+        binarized to 1, and scores less than or equal to `threshold` will be binarized to 0. Default is 4.
+    Returns
+    -------
+    filtered_scores : dict
+        A dictionary containing the filtered and binarized PSS scores, where the keys are record IDs and the
+        values are the corresponding scores.
+    """
+    scores = load_pss_labels(filename, threshold)
+    filtered_scores = filter_pss_labels(scores.iloc[:, 1:], valid_recs)
+    return filtered_scores
+
+
+
+def compute_average_stai_scores(path='Data/STAI_grading.xlsx'):
+    """
+    Compute the average scores of subjects from the raw scores.
+    Parameters
+    ----------
+    path : str
+        Path to the raw scores excel file.
+    Returns
+    -------
+    scores_df : pandas.DataFrame
+        DataFrame containing the average scores of subjects
+    """
+
+    y1_text = 'Total score Y1'
+    y2_text = 'Total score Y2'
+    columns = ['SubjectNo', 'D1Y1', 'D2Y1', 'J1Y1', 'J2Y1', 'D1Y2',
+               'D2Y2', 'J1Y2', 'J2Y2', 'AVGD1', 'AVGD2', 'AVGJ1', 'AVGJ2']
+
+    try:
+        xl = pd.ExcelFile(path)
+    except FileNotFoundError:
+        logging.error(f"File not found: {path}")
+        return
+
+    n_subjects = len(xl.sheet_names)-2
+    scores = []
+
+    sheet_names = xl.sheet_names.copy()
+    sheet_names.remove('MAL')
+    sheet_names.remove('Rating 1-10')
+
+    for sheet_name in sheet_names:
+        sheet = xl.parse(sheet_name)
+        y1_values = sheet.loc[sheet[sheet.columns[0]] == y1_text].iloc[:, 1:].to_numpy()[
+            0][::2]
+        y2_values = sheet.loc[sheet[sheet.columns[0]] == y2_text].iloc[:, 1:].to_numpy()[
+            0][::2]
+        avg_d1 = np.mean(np.hstack((y1_values[0], y2_values[0])))
+        avg_d2 = np.mean(np.hstack((y1_values[1], y2_values[1])))
+        avg_j1 = np.mean(np.hstack((y1_values[2], y2_values[2])))
+        avg_j2 = np.mean(np.hstack((y1_values[3], y2_values[3])))
+        scores.append(
+            np.hstack((y1_values, y2_values, avg_d1, avg_d2, avg_j1, avg_j2)))
+    scores_df = pd.DataFrame(scores, columns=columns[1:])
+    scores_df.insert(0, columns[0], range(1, n_subjects+1))
+    return scores_df
+
+def compute_stai_score_labels(scores, low_cutoff, high_cutoff):
+    """
+    Convert scores to labels based on low and high cutoffs.
+    Parameters
+    ----------
+    scores : pd.DataFrame
+        Dataframe containing the scores for all subjects.
+    low_cutoff : int, optional
+        The lower cutoff value. Default is 37.
+    high_cutoff : int, optional
+        The upper cutoff value. Default is 45.
+    Returns
+    -------
+    numpy.ndarray
+        An array with shape `(n_subjects, 4)` containing labels.
+    """
+
+    labels = {}
+    for i in range(scores.shape[0]):
+        for j in range(v.NUM_SESSIONS*v.NUM_RUNS):
+
+            label = int(scores.iloc[i, j+8]
+                        in range(low_cutoff, high_cutoff+1))
+            if scores.iloc[i, j+8] > high_cutoff:
+                label = 2
+            subject = i + 1
+            session = j//v.NUM_RUNS + 1
+            run = j % v.NUM_RUNS + 1
+            key = f'P{str(subject).zfill(3)}_S{str(session).zfill(3)}_{str(run).zfill(3)}'
+            labels[key] = label
+    return labels
+
+def get_stai_labels(valid_recs, path='Data/STAI_grading.xlsx', low_cutoff=37, high_cutoff=45):
+    """
+    Get labels for valid recordings.
+    Parameters
+    ----------
+    valid_recs : list
+        List of valid recordings.
+    path : str, optional
+        Path to the raw scores excel file. Default is 'Data/STAI_grading.xlsx'.
+    low_cutoff : int, optional
+        The lower cutoff value. Default is 37.
+    high_cutoff : int, optional
+        The upper cutoff value. Default is 45.
+    Returns
+    -------
+    dict
+        Dictionary containing labels for valid recordings.
+    """
+    scores = compute_average_stai_scores(path)
+    labels = compute_stai_score_labels(scores, low_cutoff, high_cutoff)
+    return labels
